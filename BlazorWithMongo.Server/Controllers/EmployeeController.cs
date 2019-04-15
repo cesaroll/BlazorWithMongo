32a6f153194832dc9801ea80716ef7a80b3fdf59
Cesar Lopez <cesarl@outlook.com>
2019-04-14 19:41:55 -0600

    Creating Employee Controller

---
+++ BlazorWithMongo.Server/Controllers/EmployeeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlazorWithMongo.Server.DataAccess;
+using BlazorWithMongo.Shared.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BlazorWithMongo.Server.Controllers
+{
+    [Route("api/[controller]")]
+    public class EmployeeController : Controller
+    {
+        private EmployeeDataAccessLayer EmployeeDAL { get; set; } = new EmployeeDataAccessLayer();
+
+        // GET api/Employee/Index
+        [HttpGet]
+        [Route("api/Employee/Index")]
+        public IEnumerable<Employee> Index()
+        {
+            return EmployeeDAL.GetAllEmployees();
+        }
+
+        /*
+        // GET api/<controller>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+        */
+    }
+}
